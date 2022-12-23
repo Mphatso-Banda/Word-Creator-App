5a23c2e585a105ef3efa4383534121603462ecb1
Mphatso Banda <M.Banda@tnm.co.mw>
2022-12-23 08:38:10 +0200

    answer fun added

--- app/src/main/java/com/tc/apps/wordcreator/viewmodels/SplashViewModel.kt
+++ app/src/main/java/com/tc/apps/wordcreator/viewmodels/SplashViewModel.kt
@@ -6,28 +6,31 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.tc.apps.wordcreator.WordsContainer
 
-class SplashViewModel():ViewModel() {
+class SplashViewModel() : ViewModel() {
     private val wordsContainer = WordsContainer()
 
-    private val _score =  MutableLiveData<Int>()
+    private val _score = MutableLiveData<Int>()
     val score: LiveData<Int> get() = _score
 
     private val _letter1 = MutableLiveData<String>()
     val letter1: LiveData<String> get() = _letter1
 
     private val _letter2 = MutableLiveData<String>()
-    val letter2 : LiveData<String> get() = _letter2
+    val letter2: LiveData<String> get() = _letter2
 
     private val _letter3 = MutableLiveData<String>()
-    val letter3 : LiveData<String> get() = _letter3
+    val letter3: LiveData<String> get() = _letter3
 
     private val _letter4 = MutableLiveData<String>()
-    val letter4 : LiveData<String> get() = _letter4
+    val letter4: LiveData<String> get() = _letter4
 
     private val _letter5 = MutableLiveData<String>()
-    val letter5 : LiveData<String> get() = _letter5
+    val letter5: LiveData<String> get() = _letter5
 
-    private fun getButtonLetter(){
+    private val _finalAnswer = MutableLiveData<String>()
+    val finalAnswer: LiveData<String> get() = _finalAnswer
+
+    private fun getButtonLetter() {
         val nextWord = shuffleWord()
 
         _letter1.value = nextWord[0].toString()
@@ -37,17 +40,18 @@ class SplashViewModel():ViewModel() {
         _letter5.value = nextWord[4].toString()
 
     }
-// shuffle the word
+
+    // shuffle the word
     private fun shuffleWord(): CharArray {
         val word = getData().toCharArray()
-            word.shuffle()
+        word.shuffle()
         Log.d("SPLASH", word[0].toString())
         return word
     }
 
-//returns a word from Words Container
-   private fun getData():String{
-     val word = wordsContainer.getWords()
+    //returns a word from Words Container
+    private fun getData(): String {
+        val word = wordsContainer.getWords()
 
         val randomMap = word.random()
         val mapKey = randomMap.map { it.key }
@@ -56,9 +60,10 @@ class SplashViewModel():ViewModel() {
         return mapKey[0]
     }
 
-    fun answer(){
+    fun answer(s: String) {
         val answer = StringBuilder()
-
+        answer.append(s)
+        _finalAnswer.value = answer.toString()
     }
 
     init {
