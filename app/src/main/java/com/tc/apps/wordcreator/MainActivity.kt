fdda7ccaee4f0192501997f7abb7e20ec88c37fb
Tamando Chigwembere <chigwemberetamando@gmail.com>
2022-12-29 15:02:42 +0200

    splashscreen addition

--- app/src/main/java/com/tc/apps/wordcreator/MainActivity.kt
+++ app/src/main/java/com/tc/apps/wordcreator/MainActivity.kt
@@ -3,6 +3,7 @@ package com.tc.apps.wordcreator
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
 import android.util.Log
 import android.view.Gravity
 import android.view.View
---
+++ app/src/main/java/com/tc/apps/wordcreator/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.tc.apps.wordcreator
+
+import android.content.Context
+import android.content.Intent
+import android.content.res.AssetManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+
+        var imageView = findViewById<ImageView>(R.id.imageView2) as ImageView
+
+        imageView.alpha = 0f
+        imageView.animate().setDuration(3000).alpha(1f).withEndAction{
+            WordsDictionary.initializeDictionary(this)
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }
+    }
+
+    companion object WordsDictionary{
+
+        private val dictionary = mutableListOf<String>()
+        fun initializeDictionary(context: Context){
+            val assetManager: AssetManager = context.assets
+            val wordpath = assetManager.open("words.txt")
+
+            wordpath.reader().useLines { lines ->
+                lines.forEach {
+                    dictionary.add(it)
+                }
+            }
+        }
+
+        fun getDictionary(): MutableList<String>{
+            return this.dictionary
+        }
+    }
+}
