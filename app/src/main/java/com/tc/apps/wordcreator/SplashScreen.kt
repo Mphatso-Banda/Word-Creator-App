340652fe0e20a83790c26cdb7b4834560713e02a
Tamando Chigwembere <chigwemberetamando@gmail.com>
2022-12-29 16:52:06 +0200

    answer text style update again

--- app/src/main/java/com/tc/apps/wordcreator/SplashScreen.kt
+++ app/src/main/java/com/tc/apps/wordcreator/SplashScreen.kt
@@ -17,7 +17,6 @@ class SplashScreen : AppCompatActivity() {
 
         var imageView = findViewById<ImageView>(R.id.imageView2) as ImageView
 
-        imageView.alpha = 0f
         imageView.animate().setDuration(1500).alpha(1f).withEndAction{
             WordsDictionary.initializeDictionary(this)
             val intent = Intent(this, MainActivity::class.java)
